d3a89654085d62ea2e8d2d2f1f3fc784e02c2e10
MarcioSousa <marciofrapeso@gmail.com>
2016-05-25 14:46:18 -0300

    commit40

---
+++ Sequencial/39_MediaPonderada.vb
@@ -0,0 +1,49 @@
+﻿'/*
+' * Author  : Antônio Paulo Neto
+' * File    : Program.cs
+' * Project : Algoritmo_39
+' * 
+' * Entrar com quatro números e imprimir a média ponderada, sabendo-se que os pesos são respectivamente: 1, 2, 3 e 4. 
+' */
+Module MediaPonderada
+
+    Sub main()
+        Dim a As Double = 0
+        Dim b As Double = 0
+        Dim c As Double = 0
+        Dim d As Double = 0
+        Dim mp As Double = 0
+
+        'Exibi no console
+        Console.Write("Entre com o primeiro número: ")
+        a = Double.Parse(Console.ReadLine)
+
+        Console.Write("Entre com o segundo número: ")
+        b = Double.Parse(Console.ReadLine)
+
+        Console.Write("Entre com o terceiro número: ")
+        c = Double.Parse(Console.ReadLine)
+
+        Console.Write("Entre com o quarto número: ")
+        d = Double.Parse(Console.ReadLine)
+
+        '//Processamento
+        mp = (a * 1 + b * 2 + c * 3 + d * 4) / 10
+
+        '//Saída
+        Console.Write("Média ponderada: " & mp)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
+'            // processamento
+'            mp = (a * 1 + b * 2 + c * 3 + d * 4) / 10;
+
+'            // saida
+'            Console.Write("\nMedia ponderada : " + mp + "\n");
+'            Console.ReadKey();
+'        }
+'    }
+'}
---
+++ Sequencial/40_GastoQuilowatts.vb
@@ -0,0 +1,38 @@
+﻿'/*
+' * Author  : Antônio Paulo Neto
+' * File    : Program.cs
+' * Project : Algoritmo_40
+' * 
+' * Antes de o racionamento de energia ser decretado, quase ninguém falava em quilowatts; mas, agora, todos incorporaram essa palavra em seu vocabulário. Sabendo-se que 100 quilowatts de energia custa um sétimo do salário mínimo, fazer um algoritmo que receba o valor do salário mínimo e a quantidade de quilowatts gasta por uma residência e calcule. Imprima 
+' * o valor em reais de cada quilowatt 
+' * o valor em reais a ser pago 
+' * o novo valor a ser pago por essa residência com um desconto de 10%. 
+' */
+Module GastoQuilowatts
+
+    Sub main()
+        Dim sm As Double = 0
+        Dim qtdade As Double = 0
+        Dim preco As Double = 0
+        Dim vp As Double = 0
+        Dim vd As Double = 0
+
+        Console.Write("Entre com o salário mínimo: ")
+        sm = Double.Parse(Console.ReadLine)
+
+        Console.Write("Entre com a quantidade em quilowatt : ")
+        qtdade = Double.Parse(Console.ReadLine)
+
+        '// processamento
+        '// divide por 7 para achar o preço de 100 kw e por 100 para achar de 1kw
+        preco = sm / 700
+        vp = preco * qtdade
+        vd = vp * 0.9
+
+        Console.Write("Preço do quilowatt: " & preco & vbCrLf)
+        Console.Write("Valor a ser pago  : " & vp & vbCrLf)
+
+        Console.ReadKey()
+    End Sub
+
+End Module
